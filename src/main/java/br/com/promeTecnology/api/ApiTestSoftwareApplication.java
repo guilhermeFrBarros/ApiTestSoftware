3d13b7cf15fdd6a4958136a457028ee7fc71f502
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-11-01 17:00:51 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/promeTecnology/api/ApiTestSoftwareApplication.java
@@ -0,0 +1,13 @@
+package br.com.promeTecnology.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/promeTecnology/api/ApiTestSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.promeTecnology.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
