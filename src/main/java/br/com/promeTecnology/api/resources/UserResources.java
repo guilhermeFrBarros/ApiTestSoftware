3c01dc4808919698e8f112d339be9e71f6adfbc7
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-11-03 15:45:57 -0300

    Classe user resource  findById

--- src/main/java/br/com/promeTecnology/api/resources/UserResources.java
+++ src/main/java/br/com/promeTecnology/api/resources/UserResources.java
@@ -1,6 +1,8 @@
 package br.com.promeTecnology.api.resources;
 
 import br.com.promeTecnology.api.domain.User;
+import br.com.promeTecnology.api.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user") //..
 public class UserResources {
 
+    @Autowired
+    private UserServices services;
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById( @PathVariable Long id ) {
+        var user = services.findById( id );
 
-        return ResponseEntity.ok().body( new User( id, "gui", "gui@gmail.com", "123") );
-
+        return ResponseEntity.ok().body( user );
     }
 
  }
