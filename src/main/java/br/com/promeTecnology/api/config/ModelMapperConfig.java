8cb756589c6054c0c829fd90fcee445a6dec9af1
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-11-07 10:37:19 -0300

    disponibilizando ModelMapper em forma de Bean

---
+++ src/main/java/br/com/promeTecnology/api/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package br.com.promeTecnology.api.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+}
