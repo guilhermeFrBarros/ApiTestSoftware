2c1ee6f620ee01728395e6581af04025ac97e104
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2023-11-07 10:37:55 -0300

    return DTO para UserResources

--- src/main/java/br/com/promeTecnology/api/resources/UserResources.java
+++ src/main/java/br/com/promeTecnology/api/resources/UserResources.java
@@ -1,7 +1,9 @@
 package br.com.promeTecnology.api.resources;
 
 import br.com.promeTecnology.api.domain.User;
+import br.com.promeTecnology.api.domain.dto.UserDTO;
 import br.com.promeTecnology.api.services.UserServices;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResources {
 
     @Autowired
+    private ModelMapper mapper;
+    @Autowired
     private UserServices services;
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById( @PathVariable Long id ) {
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id ) {
         var user = services.findById( id );
 
-        return ResponseEntity.ok().body( user );
+        return ResponseEntity.ok().body( mapper.map( user,  UserDTO.class ));
     }
 
  }
